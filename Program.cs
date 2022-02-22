c9c6e610200eafe4056186bb4a6fdc200c3d778f
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-22 21:52:41 +0530

    Finding_Max_Float

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace genericProblems
         {
             Console.WriteLine("Welcome to the generic problems");
             Program findMax = new Program();
-            findMax.maxLength(2,5,6);
+            findMax.maxLength(2.5F,8.9F,6.8F);
         }
-        private void maxLength(int value1, int value2, int value3)
+        private void maxLength(float value1, float value2, float value3)
         {
             if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
             {
