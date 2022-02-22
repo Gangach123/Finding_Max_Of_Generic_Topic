d4d0f71a32a0b6e23cf5780311abd0dddcd6b690
Gangadevi Ch <chilamakooru.gangaiiit@gmail.com>
2022-02-22 20:08:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace genericProblems
+{
+    class FindMax
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to the generic problems");
+        }
+    }
+}
