65e71c7c240c2da17b211b96bebffe09a80ffe03
Gangach123 <96557743+Gangach123@users.noreply.github.com>
2022-02-22 23:05:20 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,21 +7,22 @@ namespace genericProblems
         {
             Console.WriteLine("Welcome to the generic problems");
             Program findMax = new Program();
-            findMax.maxLength(2,5,6);
+            Console.WriteLine($"Maximum number is {findMax.maxLength(569,478, 999)}");
+            
         }
         private void maxLength(int value1, int value2, int value3)
         {
             if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
             {
-                Console.WriteLine(value1);
+                return value1;
             }
             else if (value2.CompareTo(value3) > 0 && value2.CompareTo(value1) > 0)
             {
-                Console.WriteLine(value2);
+                return value2;
             }
             else
             {
-                Console.WriteLine(value3);
+                return value3;
             }
         }
     }
