e4304536f56907f49fc40ff170791e0c8fb982f5
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-23 10:22:35 +0530

    Refractor2

--- Program.cs
+++ Program.cs
@@ -7,21 +7,27 @@ namespace genericProblems
         {
             Console.WriteLine("Welcome to the generic problems");
             Program findMax = new Program();
-            findMax.maxLength(2,5,6);
+            Console.WriteLine($"Maximum number is {findMax.maxLength<int>(569, 478, 999)}");
+            Console.WriteLine($"Maximum float number is {findMax.maxLength<float>(9.9F, 5.6F, 4.7F)}");
+            Console.WriteLine($"Maximum string is {findMax.maxLength<string>("Ganga", "Venky", "Siri")}");
+
+
         }
-        private void maxLength(int value1, int value2, int value3)
+
+        private T maxLength<T>(T value1, T value2, T value3)
+
         {
-            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
+            if (Comparer<T>.Default.Compare(value1, value2) > 0 && Comparer<T>.Default.Compare(value1, value3) > 0)
             {
-                Console.WriteLine(value1);
+                return value1;
             }
-            else if (value2.CompareTo(value3) > 0 && value2.CompareTo(value1) > 0)
+            else if (Comparer<T>.Default.Compare(value2, value1) > 0 && Comparer<T>.Default.Compare(value2, value3) > 0)
             {
-                Console.WriteLine(value2);
+                return value2;
             }
             else
             {
-                Console.WriteLine(value3);
+                return value3;
             }
         }
     }
